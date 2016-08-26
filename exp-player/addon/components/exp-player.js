620deab4e87cf5eceaa445d10e2734fdb0f13b6d
Sam Chrisinger <s.chrisinger@gmail.com>
2016-08-26 16:03:08 -0400

    WIP moving Lookit-specific code to Lookit

--- exp-player/addon/components/exp-player.js
+++ exp-player/addon/components/exp-player.js
@@ -4,10 +4,6 @@ import layout from '../templates/components/exp-player';
 import FullScreen from '../mixins/full-screen';
 import ExperimentParser from '../utils/parse-experiment';
 
-let {
-    $
-} = Ember;
-
 export default Ember.Component.extend(FullScreen, {
     layout: layout,
 
@@ -22,57 +18,8 @@ export default Ember.Component.extend(FullScreen, {
     displayFullscreen: false,
     fullScreenElementId: 'experiment-player',
 
-    allowExit: false,
-    hasAttemptedExit: false,
-    _registerHandlers() {
-        $(window).on('beforeunload', () => {
-            if (!this.get('allowExit')) {
-                this.set('hasAttemptedExit', true);
-                this.send('exitFullscreen');
-                let toast = this.get('toast');
-                toast.warning('To leave the study early, press F1 and then select a privacy level for your videos');
-                return `
-If you're sure you'd like to leave this study early
-you can do so now.
-
-We'd appreciate it if before you leave you fill out a
-very breif exit survey letting us know how we can use
-any video captured during this session. Press 'Stay on
-this Page' and you will be prompted to go to this
-exit survey.
-
-If leaving this page was an accident you will be
-able to continue the study.
-`;
-            }
-            return null;
-        });
-        $(window).on('keyup', (e) => {
-            console.log(e.which);
-            if (e.which === 112) {
-                this.send('exitEarly');
-            }
-        });
-    },
-    _removeHandlers() {
-        $(window).off('keypress');
-        $(window).off('beforeunload');
-    },
-    onFrameIndexChange: Ember.observer('frameIndex', function() {
-        var max = this.get('frames.length') - 1;
-        var frameIndex = this.get('frameIndex');
-        if (frameIndex === max) {
-            this._removeHandlers();
-        }
-    }),
-    willDestroy() {
-        this._super(...arguments);
-        this._removeHandlers();
-    },
-
     init: function() {
         this._super(...arguments);
-        this._registerHandlers();
 
         var parser = new ExperimentParser({
             structure: this.get('experiment.structure'),
